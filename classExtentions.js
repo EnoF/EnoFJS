591f56be7ee47f36cdcc618ba9bf61e037dd610e
Andy Tang <andy@provictores.com>
2012-11-11 22:43:29 +0100

    Protected variables are copied from the Parent to Child while extending

---
+++ classExtentions.js
@@ -0,0 +1,18 @@
+﻿Function.prototype.extend = function (Super) {
+
+    var OriginalClass = this;
+
+    function ClassWrapper() {
+        var instance = new OriginalClass(),
+            superInstance = new Super(),
+            originalProtected = instance.protected,
+            superProtected = superInstance.protected;
+
+        $.extend(superProtected, originalProtected);
+        $.extend(originalProtected, superProtected);
+
+        return instance;
+    }
+
+    return ClassWrapper;
+};
---
+++ test/spec/classExtentions.js
@@ -0,0 +1,43 @@
+﻿describe('class extentions', function () {
+
+    function Parent() {
+        var protected = {
+            foo: "FOO",
+            bar: function () {
+                return "BAR";
+            },
+            override: "WAIT"
+        };
+
+        this.protected = protected;
+    }
+
+    function Child() {
+        var protected = {
+            food: "FOOD",
+            getFooBar: function () {
+                return protected.foo + " " + protected.bar();
+            },
+            override: "DONE"
+        };
+
+        this.protected = protected;
+
+        this.getProtected = function () {
+            return protected;
+        };
+    }
+    Child = Child.extend(Parent);
+
+    it('should copy protected variables from parent to child', function () {
+        var child = new Child();
+
+        expect(child.getProtected().getFooBar()).toEqual("FOO BAR");
+    });
+
+    it('should be able to override variables', function () {
+        var child = new Child();
+
+        expect(child.getProtected().override).toEqual("DONE");
+    });
+});
